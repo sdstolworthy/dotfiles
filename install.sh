201b31ca4bcd73d7316e8d87269ddcb37dad5dc6
Spencer Stolworthy <sdstolworthy@gmail.com>
2023-04-22 00:49:59 -0600

    updated install script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 ln -s `pwd`/coc.nvim $HOME/.config/nvim/coc.nvim
 ln -s `pwd`/coc-settings.json $HOME/.config/nvim/coc-settings.json
-ln -s `pwd`/init.vim $HOME/.config/nvim/init.vim
+ln -s `pwd`/vimrc $HOME/.config/nvim/init.vim
 ln -s `pwd`/profile $HOME/.profile
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 mkdir -p $ZSH_CUSTOM/plugins
