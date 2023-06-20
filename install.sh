19cabc94e5305d39e67153c66acb105cf4da1cad
Spencer Stolworthy <sstol@amazon.com>
2023-06-20 12:03:21 -0700

    update install script, vimrc

--- install.sh
+++ install.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+mkdir -p $HOME/.config/nvim
+
 ln -s `pwd`/coc.nvim $HOME/.config/nvim/coc.nvim
 ln -s `pwd`/coc-settings.json $HOME/.config/nvim/coc-settings.json
 ln -s `pwd`/vimrc $HOME/.config/nvim/init.vim
 ln -s `pwd`/profile $HOME/.profile
+ln -s `pwd`/coc $HOME/.config/nvim/coc
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 mkdir -p $ZSH_CUSTOM/plugins
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
