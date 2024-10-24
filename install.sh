5e07cabdd0632ada48faa06f001af1a5a044f71c
Spencer Stolworthy <sdstolworthy@gmail.com>
2024-10-24 00:27:28 -0700

    removed old configs

--- install.sh
+++ install.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-mkdir -p $HOME/.config/nvim/lua
+mkdir -p $HOME/.config/nvim
 mkdir -p $HOME/.config/alacritty
-ln -s `pwd`/lua/*.lua $HOME/.config/nvim/lua/
+ln -s `pwd`/lua $HOME/.config/nvim/lua
 ln -s `pwd`/alacritty.toml $HOME/.config/alacritty/alacritty.toml
-
 ln -s `pwd`/tmux.conf $HOME/.tmux.conf
-ln -s `pwd`/coc.nvim $HOME/.config/nvim/coc.nvim
-ln -s `pwd`/coc-settings.json $HOME/.config/nvim/coc-settings.json
-ln -s `pwd`/vimrc $HOME/.config/nvim/init.vim
+ln -s `pwd`/init.lua $HOME/.config/nvim/init.lua
 ln -s `pwd`/profile $HOME/.profile
 ZSH_CUSTOM=$HOME/.oh-my-zsh/custom
 mkdir -p $ZSH_CUSTOM/plugins
