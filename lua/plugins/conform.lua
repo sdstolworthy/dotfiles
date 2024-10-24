089d1a2b85d6d391c191d213cb9ea66f807818ee
Spencer Stolworthy <sdstolworthy@gmail.com>
2024-10-24 00:52:26 -0700

    add formatter config

---
+++ lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+	"stevearc/conform.nvim",
+	opts = function()
+		local conform = require("conform")
+
+		vim.keymap.set("n", "<space>as", function()
+			conform.format({ async = true, lsp_fallback = true })
+		end)
+		conform.setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				-- Conform will run multiple formatters sequentially
+				python = { "isort", "black" },
+				-- Use a sub-list to run only the first available formatter
+				javascript = { "prettier" },
+				typescript = { "prettier" },
+			},
+		})
+	end,
+}
