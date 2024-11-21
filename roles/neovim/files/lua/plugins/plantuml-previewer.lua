a1e607ef96c04e34813a81ae9000e9eddc76280e
Spencer Dean Stolworthy <sstol@amazon.com>
2024-11-21 13:12:12 +0900

    add plantuml

---
+++ roles/neovim/files/lua/plugins/plantuml-previewer.lua
@@ -0,0 +1,7 @@
+vim.g.plantuml_previewer.plantuml_jar_path = "/opt/bin/plantuml"
+return {
+	"weirongxu/plantuml-previewer.vim",
+	opts = function()
+		vim.g.plantuml_previewer.plantuml_jar_path = "/opt/bin/plantuml"
+	end,
+}
