0f929bf114f51219d2ca864991b043304ada0a97
Spencer Stolworthy <sdstolworthy@gmail.com>
2024-12-21 11:26:25 -0700

    add profiled task

--- roles/neovim/files/lua/plugins/plantuml-previewer.lua
+++
@@ -1,7 +0,0 @@
-vim.g.plantuml_previewer.plantuml_jar_path = "/opt/bin/plantuml"
-return {
-	"weirongxu/plantuml-previewer.vim",
-	opts = function()
-		vim.g.plantuml_previewer.plantuml_jar_path = "/opt/bin/plantuml"
-	end,
-}
