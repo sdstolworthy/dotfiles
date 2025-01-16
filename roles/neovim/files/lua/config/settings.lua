27e0c523dbb78987ff5a1e15c1b01ac5b8dd5ecc
Spencer Stolworthy <sstol@amazon.com>
2025-01-16 22:58:46 +0000

    fix tabstops

--- roles/neovim/files/lua/config/settings.lua
+++ roles/neovim/files/lua/config/settings.lua
@@ -6,9 +6,6 @@ vim.opt.relativenumber = false
 vim.opt.showmode = false
 
 vim.opt.expandtab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
