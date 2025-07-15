d368c950c2e32b13ec45aae43fd4f9b6088ee0f8
Spencer Stolworthy <sdstolworthy@gmail.com>
2025-07-15 08:01:40 -0700

    fix: migrate to nvim 0.11

---
+++ roles/neovim/files/lua/plugins/q.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    name = 'amazonq',
+    url = 'ssh://git.amazon.com/pkg/AmazonQNVim',
+    opts = {
+      ssoStartUrl = 'https://amzn.awsapps.com/start',
+      -- Note: It's normally not necessary to change default `lsp_server_cmd`.
+      -- lsp_server_cmd = {
+      --   'node',
+      --   vim.fn.stdpath('data') .. '/lazy/AmazonQNVim/language-server/build/aws-lsp-codewhisperer-token-binary.js',
+      --   '--stdio',
+      -- },
+    },
+  },
+}
+
