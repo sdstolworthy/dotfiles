a5c1e8ab464c2da45da9d65a70ec6b6af6f0b49d
Spencer Stolworthy <sdstolworthy@gmail.com>
2025-07-15 20:59:05 -0700

    add themes

--- roles/neovim/files/lua/plugins/colorschemes.lua
+++ roles/neovim/files/lua/plugins/colorschemes.lua
@@ -1,10 +1,20 @@
 return {
-  {
-    "Mofiqul/dracula.nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("dracula")
-    end
-  }
+	{
+		"Mofiqul/dracula.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("dracula")
+		end,
+	},
+	{
+		"folke/tokyonight.nvim",
+		lazy = false,
+		priority = 1000,
+	},
+	{
+		"morhetz/gruvbox",
+		lazy = false,
+		priority = 1000,
+	},
 }
