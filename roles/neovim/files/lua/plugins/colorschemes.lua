b7d7dc1aae98ed7000076319192636ed54ba4641
Spencer Stolworthy <sdstolworthy@gmail.com>
2025-11-04 22:08:03 -0800

    add colorscheme update

--- roles/neovim/files/lua/plugins/colorschemes.lua
+++ roles/neovim/files/lua/plugins/colorschemes.lua
@@ -3,14 +3,14 @@ return {
 		"Mofiqul/dracula.nvim",
 		lazy = false,
 		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("dracula")
-		end,
 	},
 	{
 		"folke/tokyonight.nvim",
 		lazy = false,
 		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("tokyonight-night")
+		end,
 	},
 	{
 		"morhetz/gruvbox",
