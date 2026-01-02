b13926117c07425f0ad2a46f384d76cc44b7ab1a
Spencer Stolworthy <sdstolworthy@gmail.com>
2026-01-02 09:49:13 -0500

    add asdf; fix fira and zellij install

--- roles/neovim/files/lua/plugins/treesitter.lua
+++ roles/neovim/files/lua/plugins/treesitter.lua
@@ -1,24 +1,22 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "vimdoc",
-        "go",
-        "lua",
-        "java",
-        "rust",
-        "typescript",
-      },
-      sync_install = false,
-      auto_install = true,
-      indent = {
-        enable = true
-      },
-      highlight = {
-        enable = true,
-      },
-    })
-  end
+  opts = {
+    ensure_installed = {
+      "vimdoc",
+      "go",
+      "lua",
+      "java",
+      "rust",
+      "typescript",
+    },
+    sync_install = false,
+    auto_install = true,
+    indent = {
+      enable = true
+    },
+    highlight = {
+      enable = true,
+    },
+  },
 }
