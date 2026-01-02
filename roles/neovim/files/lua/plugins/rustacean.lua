9ba10f5a92fc73765865488a9e588f819e2e9530
Spencer Stolworthy <sstol@amazon.com>
2026-01-02 12:30:34 -0700

    add rust debug config

--- roles/neovim/files/lua/plugins/rustacean.lua
+++ roles/neovim/files/lua/plugins/rustacean.lua
@@ -1,5 +1,25 @@
 return {
   'mrcjkb/rustaceanvim',
-  version = '^6', -- Recommended
-  ft = { 'rust' },
+  version = '^6',
+  lazy = false,
+  init = function()
+    vim.g.rustaceanvim = {
+      server = {
+        default_settings = {
+          ["rust-analyzer"] = {
+            cargo = {
+              features = "all",
+              allFeatures = true,
+            },
+          },
+        },
+      },
+      dap = {
+        adapter = require("rustaceanvim.config").get_codelldb_adapter(
+          vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+          vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/lldb/lib/liblldb.dylib"
+        ),
+      },
+    }
+  end,
 }
