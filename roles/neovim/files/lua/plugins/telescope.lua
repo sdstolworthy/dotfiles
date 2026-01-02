d79a848fd147ce336681e4aab3af2f7385e0017b
Spencer Stolworthy <sstol@amazon.com>
2026-01-02 11:01:47 -0700

    feat: qol improvements for neovim

--- roles/neovim/files/lua/plugins/telescope.lua
+++ roles/neovim/files/lua/plugins/telescope.lua
@@ -4,10 +4,16 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
         { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-telescope/telescope-ui-select.nvim",
     },
     config = function()
-        require("telescope").setup({})
+        require("telescope").setup({
+            extensions = {
+                ["ui-select"] = { require("telescope.themes").get_dropdown() },
+            },
+        })
         require("telescope").load_extension("fzf")
+        require("telescope").load_extension("ui-select")
     end,
     keys = {
         { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find files" },
