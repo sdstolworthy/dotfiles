c48792011b0cc8065317d3bc15315109d89396c9
Spencer Stolworthy <sstol@amazon.com>
2026-01-02 12:19:25 -0700

    add flash

---
+++ roles/neovim/files/lua/plugins/flash.lua
@@ -0,0 +1,9 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{ "s", function() require("flash").jump() end, mode = { "n", "x", "o" }, desc = "Flash" },
+		{ "S", function() require("flash").treesitter() end, mode = { "n", "x", "o" }, desc = "Flash Treesitter" },
+	},
+}
