66b54d4cad0cb53f111efe6d0a4cd5cca0fe8510
Spencer Stolworthy <sdstolworthy@gmail.com>
2026-01-03 21:02:37 -0700

    clipboard

--- roles/neovim/files/lua/config/settings.lua
+++ roles/neovim/files/lua/config/settings.lua
@@ -33,3 +33,16 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+-- OSC 52 clipboard
+vim.g.clipboard = {
+  name = 'OSC 52',
+  copy = {
+    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+  },
+  paste = {
+    ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+    ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+  },
+}
